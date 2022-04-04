8667b5164153713551cb8f782aedd5fff04c908a
Paolo Elia <92871203+ImPaoloEliaX0@users.noreply.github.com>
2022-04-04 11:03:36 +0200

    esercizio impostato correttamente

---
+++ laravel/resources/views/comics/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="{{ route('comics.create') }}">Crea Fumetto</a>
+
+    @foreach($comics as $comic)
+    <ul>
+        <li>{{ $comic['title'] }}</li>
+        <li>{{ substr($comic['description'], 0, 100) }}</li>
+        <li>{{ $comic['thumb'] }}</li>
+        <li>{{ $comic['price'] }}</li>
+        <li>{{ $comic['series'] }}</li>
+        <li>{{ $comic['sale_date'] }}</li>
+        <li>{{ $comic['type'] }}</li>
+    </ul>
+    <a href="{{ route('comics.show', $comic->id) }}">Dettagli</a>
+    <a href="{{ route('comics.edit', $comic->id) }}">Modifica</a>
+    
+    <form 
+        action="{{ route('comics.destroy', $comic->id) }}" 
+        method="POST">
+        @METHOD('delete')
+        @csrf
+            <button type="submit">Elimina</button>
+    </form>
+    @endforeach
+</body>
+</html>
